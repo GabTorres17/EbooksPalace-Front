996556dade06596b42623b12a99bb9c5b72206dc
Maira Zamer <mairazamer@gmail.com>
2024-06-13 22:15:18 -0300

    ola

---
+++ EbooksPalace/src/components/Carrousel/Carrousel.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const Carrousel = () => {
+
+    return(
+        <div>
+            <Carousel>
+                
+            </Carousel>
+        </div>
+    )
+
+}
+
+
+// function Example() {
+
+
+
+
+//     return (
+//         <Carousel controls indicators >
+//             <CarouselItem>
+//                 <Image className="d-block w-100" src={ReactImg} alt="slide 1" />
+//             </CarouselItem>
+//             <CarouselItem>
+//                 <Image className="d-block w-100" src={VueImg} alt="slide 2" />
+//             </CarouselItem>
+//             <CarouselItem>
+//                 <Image className="d-block w-100" src={AngularImg} alt="slide 3" />
+//             </CarouselItem>
+//         </Carousel>
+//     );
+// }
+
+export default Carrousel;
---
+++ EbooksPalace/src/pages/Langing/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SearchBar from '../../components/SearchBar/SearchBar'
+import Example from '../../components/Carrousel/Carrousel'
+
+const Landing = () => {
+  return (
+    <div>
+      <SearchBar />
+      <Example />
+      Esta es la landinggggggg pageeeeeeeeeeeeeeee
+    </div>
+  )
+}
+
+export default Landing
