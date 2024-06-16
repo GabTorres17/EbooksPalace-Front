80cdeb9ff1396ffd802d6753377e38ba3051ec83
Maira Zamer <mairazamer@gmail.com>
2024-06-15 23:43:00 -0300

    redux

--- EbooksPalace/src/pages/Detail/Detail.jsx
+++ EbooksPalace/src/pages/Detail/Detail.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from '../../components/Nav/Nav'
 import './Detail.css'
 import portada from '../../components/Images/portada.png'
+import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { getBooksId } from '../../redux/actions'
 
 const Detail = () => {
+
+//   const { id } = useParams();
+//   const dispatch = useDispatch();
+
+// useEffect(() => {
+//   dispatch(getBooksId(id))
+// }, [id] );
+
   return (
     <div>
       <NavBar />
       <div className="detalle-producto">
         <div className="imagen">
-        <br/>
-        <br/>
-        <br/>
+          <br />
+          <br />
+          <br />
           <img src={portada} />
           <h3>Descripcion: </h3>
         </div>
         <div className="info">
-        <br/>
-        <br/>
+          <br />
+          <br />
           <h2>Titulo: </h2>
           <p>Precio: </p>
           <button>Agregar al Carrito</button>
