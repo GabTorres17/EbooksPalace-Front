239c951bc396a81d59c65e25030e296a26235df4
EdsonCasta <dedsoncasta07@gmail.com>
2024-06-18 10:54:12 -0500

    corrections filters

--- EbooksPalace/src/components/Nav/Nav.jsx
+++ EbooksPalace/src/components/Nav/Nav.jsx
@@ -1,36 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 import logo from '../Images/logo.png';
 import carrito from '../Images/carrito.png';
 import SearchBar from '../SearchBar/SearchBar';
-
+import { BookContext } from '../../BookContext';
 
 const NavBar = () => {
-    return (
-        <nav className="navbar">
-            <div className="navbar-left">
-                <Link to="/">
-                    <img src={logo} alt="Logo" className="logo" />
-                </Link>
-            </div>
 
-            <div className="navbar-left">
-                <Link to="/home">
-                    <button>home</button>
-                </Link>
-            </div>
-            <SearchBar />
-            <div >
-                <Link to="/form">
-                    <button>Crear Libro</button>
-                </Link>
-            </div>
-            <div className="navbar-right">
-                <img src={carrito} alt="Carrito de Compras" className="shopping-cart" />
-            </div>
-        </nav>
-    );
+    const { handleSearch } = useContext(BookContext);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <Link to="/">
+          <img src={logo} alt="Logo" className="logo" />
+        </Link>
+      </div>
+      <div className="navbar-left">
+        <Link to="/home">
+          <button>Home</button>
+        </Link>
+      </div>
+      <SearchBar onSearch={handleSearch} />
+      <div>
+        <Link to="/form">
+          <button>Crear Libro</button>
+        </Link>
+      </div>
+      <div className="navbar-right">
+        <img src={carrito} alt="Carrito de Compras" className="shopping-cart" />
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
--- EbooksPalace/src/pages/Home/Home.jsx
+++ EbooksPalace/src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Cards from "../../components/Cards/Cards";
 import NavBar from "../../components/Nav/Nav";
-import Filters from "../../components/Filters/Filters";
 import './Home.css';
 
 const Home = () => {
