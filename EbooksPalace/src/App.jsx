37f292b9b92f900d798f31a3640da05b5fa8b1ec
GerIgRuizDiaz <germanruizdi@gmail.com>
2024-06-19 17:15:13 -0300

    CartAdvanced

--- EbooksPalace/src/App.jsx
+++ EbooksPalace/src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home/Home.jsx'
 import Detail from './pages/Detail/Detail.jsx'
 import Landing from './pages/Langing/Landing.jsx'
 import Form from './pages/Form/Form.jsx'
-
+import Cart from './pages/Cart/Cart.jsx'
 function App() {
   
   return (
@@ -14,6 +14,7 @@ function App() {
           <Route path='/home' element={<Home/>}/>
           <Route path='/detail/:id' element={<Detail/>}/>
           <Route path='/form' element={<Form/>}/>
+          <Route path='/cartbuy' element={<Cart></Cart>}/>
         </Routes>
       </div>
     </>
--- EbooksPalace/src/redux/reducers.js
+++ EbooksPalace/src/redux/reducers.js
@@ -1,6 +1,10 @@
+import { ADD_TO_CART, REMOVE_FROM_CART, UPDATE_QUANTITY, CLEAR_CART } from './actions';
+
+
 const initialState = {
   books: [],
   totalPages: 1,
+  cartBuy: []
 };
 
 const booksReducer = (state = initialState, action) => {
@@ -15,5 +19,37 @@ const booksReducer = (state = initialState, action) => {
       return state;
   }
 };
+const initialCartState = [];
 
-export default booksReducer;
+const cartReducer = (state = initialCartState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const itemInCart = state.find(item => item.id === action.payload.id);
+      if (itemInCart) {
+        return state.map(item =>
+          item.id === action.payload.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      }
+      return [...state, { ...action.payload, quantity: 1 }];
+    case REMOVE_FROM_CART:
+      return state.filter(item => item.id !== action.payload);
+    case UPDATE_QUANTITY:
+      return state.map(item =>
+        item.id === action.payload.id
+          ? { ...item, quantity: action.payload.quantity }
+          : item
+      );
+    case CLEAR_CART:
+      return [];
+    default:
+      return state;
+  }
+};
+
+
+
+export { booksReducer, cartReducer };
+
+ 
