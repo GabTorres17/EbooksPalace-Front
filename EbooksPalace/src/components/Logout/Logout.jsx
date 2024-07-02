d2b8d099fad95c6f30c2969acf9507fd34a485cb
Maira Zamer <mairazamer@gmail.com>
2024-07-02 15:38:36 -0300

    toast3

--- EbooksPalace/src/components/Logout/Logout.jsx
+++ EbooksPalace/src/components/Logout/Logout.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { ToastContainer, toast, Bounce } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Bounce } from 'react-toastify'; // Importar Bounce si no se ha importado aún
 
 export const LogoutButton = () => {
     const { logout } = useAuth0();
 
-    return <button onClick={() => logout({ returnTo: window.location.origin })}>Cerrar Sesión</button>
+    const handleLogout = () => {
+        logout({ returnTo: window.location.origin });
+        toast.success('¡Se cerró la sesión correctamente!', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce, // Usar la transición Bounce
+        });
+    };
+
+    return (
+        <>
+            <button onClick={handleLogout}>Cerrar Sesión</button>
+            <ToastContainer />
+        </>
+    );
 }
